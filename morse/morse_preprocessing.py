f867c276ba76be93d9c9b51e15faa43de0b5955a
Nikolai Lesovich <lesovich2005@mail.ru>
2025-04-19 12:30:07 +0500

    Levenshtein Mean = 0.39750

--- morse/morse_preprocessing.py
+++ morse/morse_preprocessing.py
@@ -4,34 +4,37 @@ import torchaudio
 
 
 class MorsePreprocessing(nn.Module):
-    def __init__(self, n_fft=1024, hop_length=256, power=2.0):
+    def __init__(self, sr=8000, window_duration=0.02, n_fft=1024, hop_length=256, power=2.0, alpha=0.1, margin=5):
         super().__init__()
         self.spectrogram = torchaudio.transforms.Spectrogram(
             n_fft=n_fft,
             hop_length=hop_length,
-            power=power
+            power=power,
         )
+
+        self.alpha = alpha
+        self.margin = margin
 
     def normalize_amplitude(self, waveform):
         max_val = waveform.abs().max()
+
         return waveform / max_val if max_val > 0 else waveform
 
-    def normalize_spectrogram(self, spec):
-        energy = spec.mean(dim=1).squeeze(0)
-        mask = energy > -80
+    def threshold_spectrogram(self, spec):
+        energy_per_bin = spec.mean(dim=2)
+        peak_bin = torch.argmax(energy_per_bin, dim=1)
+        peak_mean = spec[0, peak_bin.item(), :].mean()
 
-        if mask.sum() == 0:
-            return spec
+        start_bin = max(0, peak_bin.item() - self.margin)
+        end_bin = min(spec.shape[1], peak_bin.item() + self.margin)
 
-        active = spec[:, :, mask]
-        mean = active.mean()
-        std = active.std()
+        spec = spec[:, start_bin:end_bin, :]
 
-        return (spec - mean) / (std + 1e-6)
+        return torch.where(spec >= peak_mean * self.alpha, spec, 0.0)
 
     def forward(self, waveform):
-        #waveform = self.normalize_amplitude(waveform)
         spec = self.spectrogram(waveform)
-        spec = self.normalize_spectrogram(spec)
+        spec = self.threshold_spectrogram(spec)
+        spec = self.normalize_amplitude(spec)
 
         return spec
